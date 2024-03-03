b137b688e3bc6e2aefda862ef95b8044cc22ad17
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-03-04 00:05:12 +0100

    Add configpath and some user feedback messages

--- src/main/java/net/onelitefeather/labyrinth/commands/CenterCommand.java
+++ src/main/java/net/onelitefeather/labyrinth/commands/CenterCommand.java
@@ -1,37 +1,46 @@
 package net.onelitefeather.labyrinth.commands;
 
-import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.onelitefeather.labyrinth.Labyrinth;
+import net.onelitefeather.labyrinth.utils.Constants;
 import org.bukkit.entity.Player;
+import org.incendo.cloud.annotations.Argument;
 import org.incendo.cloud.annotations.Command;
 
 import java.util.regex.Matcher;
-import static net.onelitefeather.labyrinth.Labyrinth.pattern;
-import static org.bukkit.plugin.java.JavaPlugin.getPlugin;
 
 @Command("labyrinth")
 public class CenterCommand {
 
-    private static final String CONFIG_PATH = "%s.%s";
-
     private final Labyrinth labyrinth;
-
-
 
     public CenterCommand(Labyrinth labyrinth) {
         this.labyrinth = labyrinth;
     }
+
     @Command("center <zone>")
-    public void centerCommand(Player player, String zone) {
-        Matcher matcher = pattern.matcher(zone);
-        boolean notMatches = !(matcher.matches());
-        if (notMatches) {
-            player.sendMessage(Component.text("Only characters without symbols are allowed"));
+    public void centerCommand(Player player, @Argument(value = "zone") String zone) {
+
+        var configPath = Constants.CONFIG_ZONE_PATH.formatted(zone);
+
+        if (labyrinth.getConfig().isSet(configPath)) {
+            player.sendMessage(MiniMessage.miniMessage().deserialize(
+                    "Zone <zone> was already set!", Placeholder.unparsed("zone", zone)));
             return;
         }
-            this.labyrinth.getConfig().set(CONFIG_PATH.formatted(zone,
-                    Labyrinth.CONFIG_CENTER_LOCATION), player.getLocation());
-            labyrinth.saveConfig();
+
+        Matcher matcher = Constants.PATTERN.matcher(zone);
+        boolean notMatches = !(matcher.matches());
+        if (notMatches) {
+            player.sendMessage(MiniMessage.miniMessage().deserialize("Only characters without symbols are allowed"));
+            return;
+        }
+
+        this.labyrinth.getConfig().set(Constants.CONFIG_ZONE_CENTER_PATH.formatted(zone), player.getLocation());
+        labyrinth.saveConfig();
+        player.sendMessage(MiniMessage.miniMessage().deserialize(
+                "Zone <zone> was successfully set!", Placeholder.unparsed("zone", zone)));
     }
 
 
