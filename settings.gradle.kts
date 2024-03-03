564a378e310a719e2cf9c9c864a2af6fc0e90e94
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-03-04 00:09:36 +0100

    Add management to declutter and make it able to publish to gitlab

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
 rootProject.name = "Labyrinth"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://eldonexus.de/repository/maven-public/")
+    }
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            version("publishdata", "1.2.5-DEV")
+            plugin("publishdata", "de.chojo.publishdata").versionRef("publishdata")
+        }
+    }
+}
