4f1c45401af2ed3e559c4ac5d3cdc11b06da7273
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-03-03 21:10:28 +0100

    Add runserver and plugin.yml generator from minecrell

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import org.gradle.internal.impldep.junit.runner.Version.id
+
 plugins {
     id("java")
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+    id("xyz.jpenilla.run-paper") version "2.2.2"
 }
 
 group = "net.onelitefeather"
@@ -7,9 +11,13 @@ version = "0.0.1-SNAPSHOT" // Change
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    implementation("org.incendo:cloud-paper:2.0.0-beta.2")
+    implementation("org.incendo:cloud-annotations:2.0.0-beta.2")
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -20,6 +28,34 @@ tasks {
     compileJava {
         options.release.set(17)
         options.encoding = "UTF-8"
+    }
+    runServer {
+        minecraftVersion("1.20.4")
+    }
+}
+bukkit {
+    main = "net.onelitefeather.labyrinth.Labyrinth"
+    name = "Labyrinth"
+    version = "0.0.1-Snapshot"
+    description = "This is a prototype plugin for the Labyrinth of our Survival Server"
+    website = "https://discord.onelitefeather.net"
+    author = "OneLiteFeather"
+    apiVersion = "1.20"
+    prefix = "Labyrinth"
+
+    commands {
+        register("center") {
+            description = "Save the center (middle point) of your cycle"
+            permissionMessage = "You do not have permission to setup a cycle"
+            permission = "labyrinth.setup.center"
+            usage = "/labyrinth center"
+        }
+        register("setradius") {
+            description = "Save the second position needed for the cycle radius"
+            permissionMessage = "You do not have permission to setup the cycle radius!"
+            permission = "labyrinth.setup.setradius"
+            usage = "/labyrinth setradius"
+        }
     }
 }
 
