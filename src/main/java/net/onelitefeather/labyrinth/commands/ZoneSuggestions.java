d98e044c0c9c4c11ad646e398ba42bcdcabc59a1
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-03-15 23:59:28 +0100

    Create interface for zone tabcompletion to suggest existing zones

---
+++ src/main/java/net/onelitefeather/labyrinth/commands/ZoneSuggestions.java
@@ -0,0 +1,25 @@
+package net.onelitefeather.labyrinth.commands;
+
+import net.onelitefeather.labyrinth.Labyrinth;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.util.StringUtil;
+import org.incendo.cloud.annotations.suggestion.Suggestions;
+import org.incendo.cloud.context.CommandContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ZoneSuggestions {
+
+    @Suggestions("zone")
+    default List<String> suggestions(CommandContext<CommandSender> context, String input) {
+        ConfigurationSection zones = labyrinth().getConfig().getConfigurationSection("zones");
+        if (zones == null) {
+            return List.of();
+        }
+        return StringUtil.copyPartialMatches(input, zones.getKeys(false).stream().toList(), new ArrayList<>()) ;
+    }
+
+    Labyrinth labyrinth();
+}
