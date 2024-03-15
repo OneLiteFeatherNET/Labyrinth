16baf91cd82ba4a074a3eef10a57bcb9340a3192
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-03-15 23:55:21 +0100

    Change to record and fix command validation

--- src/main/java/net/onelitefeather/labyrinth/commands/CenterCommand.java
+++ src/main/java/net/onelitefeather/labyrinth/commands/CenterCommand.java
@@ -10,24 +10,15 @@ import org.incendo.cloud.annotations.Argument;
 import org.incendo.cloud.annotations.Command;
 
 @Command("labyrinth")
-public class CenterCommand {
-
-    private final Labyrinth labyrinth;
-
-    public CenterCommand(Labyrinth labyrinth) {
-        this.labyrinth = labyrinth;
-    }
+public record CenterCommand(Labyrinth labyrinth) implements ZoneSuggestions {
 
     @Command("center <zone>")
-    public void centerCommand(Player player, @Argument(value = "zone") String zone) {
-
-        ValidateZoneInput.validateZoneInput(player, zone, labyrinth);
-
-        this.labyrinth.getConfig().set(Constants.CONFIG_ZONE_CENTER_PATH.formatted(zone), player.getLocation());
-        labyrinth.saveConfig();
-        player.sendMessage(MiniMessage.miniMessage().deserialize(
-                "Zone <zone> was successfully set!", Placeholder.unparsed("zone", zone)));
+    public void centerCommand(Player player, @Argument(value = "zone", suggestions = "zone") String zone) {
+        if (ValidateZoneInput.validateZoneInput(player, zone, labyrinth)) {
+            this.labyrinth.getConfig().set(Constants.CONFIG_ZONE_CENTER_PATH.formatted(zone), player.getLocation());
+            labyrinth.saveConfig();
+            player.sendMessage(MiniMessage.miniMessage().deserialize(
+                    "Zone <zone> was successfully set!", Placeholder.unparsed("zone", zone)));
+        }
     }
-
-
 }
