21110c69c0cd6c1480959d508f58e78afbbd8601
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-03-15 23:58:37 +0100

    Change to record, fix command and add user message

--- src/main/java/net/onelitefeather/labyrinth/commands/SetRadiusCommand.java
+++ src/main/java/net/onelitefeather/labyrinth/commands/SetRadiusCommand.java
@@ -1,5 +1,8 @@
 package net.onelitefeather.labyrinth.commands;
 
+import net.kyori.adventure.chat.ChatType;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentLike;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.onelitefeather.labyrinth.Labyrinth;
@@ -12,29 +15,23 @@ import org.incendo.cloud.annotations.Command;
 import org.jetbrains.annotations.NotNull;
 
 
-
 @Command("labyrinth")
-public class SetRadiusCommand {
-
-    private final Labyrinth labyrinth;
-
-    public SetRadiusCommand(Labyrinth labyrinth) {
-        this.labyrinth = labyrinth;
-    }
+public record SetRadiusCommand(Labyrinth labyrinth) implements ZoneSuggestions {
 
     @Command("setradius <zone>")
-    public void setRadius(@NotNull Player player, @Argument(value = "zone" /*,suggestions = "zones"*/) String zone) {
+    public void setRadius(@NotNull Player player, @Argument(value = "zone", suggestions = "zone") String zone) {
+        if (ValidateZoneInput.validateZoneInput(player, zone, labyrinth)) {
+            Location playerLabyrinthCenterLocation = player.getLocation();
 
-        ValidateZoneInput.validateZoneInput(player, zone, labyrinth);
-        Location playerLabyrinthCenterLocation = player.getLocation();
-
-        Location location = labyrinth.getConfig().getLocation(Constants.CONFIG_ZONE_CENTER_PATH.formatted(zone));
-        if (location == null) return;
+            Location location = labyrinth.getConfig().getLocation(Constants.CONFIG_ZONE_CENTER_PATH.formatted(zone));
+            if (location == null) return;
 
 
-        labyrinth.getConfig().set(Constants.CONFIG_ZONE_RADIUS_PATH.formatted(zone), playerLabyrinthCenterLocation.distance(location));
-        labyrinth.saveConfig();
-        player.sendMessage(MiniMessage.miniMessage().deserialize(
-                "The radius for zone <zone> was successfully set!", Placeholder.unparsed("zone", zone)));
+            labyrinth.getConfig().set(Constants.CONFIG_ZONE_RADIUS_PATH.formatted(zone), playerLabyrinthCenterLocation.distance(location));
+            labyrinth.saveConfig();
+            var message = MiniMessage.miniMessage().deserialize(Constants.SET_RADIUS_MESSAGE, Placeholder.unparsed("zone", zone), Placeholder.component("prefix", Constants.PREFIX));
+            player.sendMessage(message);
+        }
+
     }
 }
