8b3006a1a3961debbd14f9c0325797a299e3073f
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-03-30 16:11:46 +0100

    Change to BlockPosition to save lines in config yaw and pitch are not needed for the zone

--- src/main/java/net/onelitefeather/labyrinth/commands/CenterCommand.java
+++ src/main/java/net/onelitefeather/labyrinth/commands/CenterCommand.java
@@ -1,10 +1,12 @@
 package net.onelitefeather.labyrinth.commands;
 
+import io.papermc.paper.math.BlockPosition;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.onelitefeather.labyrinth.Labyrinth;
 import net.onelitefeather.labyrinth.utils.Constants;
 import net.onelitefeather.labyrinth.utils.ValidateZoneInput;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.incendo.cloud.annotations.Argument;
 import org.incendo.cloud.annotations.Command;
@@ -14,6 +16,8 @@ public record CenterCommand(Labyrinth labyrinth) implements ZoneSuggestions {
 
     @Command("center <zone>")
     public void centerCommand(Player player, @Argument(value = "zone", suggestions = "zone") String zone) {
+        Location location = player.getLocation();
+        BlockPosition blockPosition = location.toBlock();
         if (ValidateZoneInput.validateZoneInput(player, zone, labyrinth)) {
             this.labyrinth.getConfig().set(Constants.CONFIG_ZONE_CENTER_PATH.formatted(zone), player.getLocation());
             labyrinth.saveConfig();
