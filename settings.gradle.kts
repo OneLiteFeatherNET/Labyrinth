2e15f8bd6c183146d5e48f30409dc1dddfe037d8
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-08-18 21:56:00 +0200

    update runPaper dependency to 2.3.0 and change to gradleup with 8.3.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,14 +13,14 @@ dependencyResolutionManagement {
 
             version("paper", "1.20.6-R0.1-SNAPSHOT")
             version("plugin.yml", "0.6.0")
-            version("run-paper", "2.2.4")
+            version("run-paper", "2.3.0")
             version("publishdata", "1.4.0")
-            version("shadow", "8.1.1")
+            version("shadow", "8.3.0")
 
             plugin("plugin.yml", "net.minecrell.plugin-yml.paper").versionRef("plugin.yml")
             plugin("run.paper", "xyz.jpenilla.run-paper").versionRef("run-paper")
             plugin("publishdata", "de.chojo.publishdata").versionRef("publishdata")
-            plugin("shadow", "com.github.johnrengelman.shadow").versionRef("shadow")
+            plugin("shadow", "com.gradleup.shadow").versionRef("shadow")
 
             library("cloudPaper", "org.incendo", "cloud-paper").version("2.0.0-beta.4")
             library("cloudAnnotations", "org.incendo", "cloud-annotations").version("2.0.0-beta.4")
