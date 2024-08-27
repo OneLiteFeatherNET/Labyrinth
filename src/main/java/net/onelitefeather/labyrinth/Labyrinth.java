03ea479b160ea1c8f2b2792d28c17428c40910a7
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-08-27 18:46:58 +0200

    remove useless imports

--- src/main/java/net/onelitefeather/labyrinth/Labyrinth.java
+++ src/main/java/net/onelitefeather/labyrinth/Labyrinth.java
@@ -7,14 +7,10 @@ import net.onelitefeather.labyrinth.commands.ToggleMobSpawnCommand;
 import net.onelitefeather.labyrinth.commands.SetRadiusCommand;
 import net.onelitefeather.labyrinth.listener.MobspawnListener;
 import net.onelitefeather.labyrinth.commands.ZoneSuggestions;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.checkerframework.checker.units.qual.C;
 import org.incendo.cloud.annotations.AnnotationParser;
-import org.incendo.cloud.bukkit.CloudBukkitCapabilities;
 import org.incendo.cloud.execution.ExecutionCoordinator;
 import org.incendo.cloud.meta.CommandMeta;
-import org.incendo.cloud.paper.LegacyPaperCommandManager;
 import org.incendo.cloud.paper.PaperCommandManager;
 
 public class Labyrinth extends JavaPlugin {
@@ -28,12 +24,8 @@ public class Labyrinth extends JavaPlugin {
 
     public void registerCommands() {
         PaperCommandManager<CommandSourceStack> commandManager = PaperCommandManager.builder()
-                .executionCoordinator(ExecutionCoordinator.simpleCoordinator())
+                .executionCoordinator(ExecutionCoordinator.asyncCoordinator())
                 .buildOnEnable(this);
-
-//        if (commandManager.hasCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
-//           commandManager.registerAsynchronousCompletions();
-//        }
 
         AnnotationParser<CommandSourceStack> annotationParser = new AnnotationParser<>(
                 commandManager,
