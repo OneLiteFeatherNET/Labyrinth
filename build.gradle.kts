d92baeb0557ee6bd26477a78ade91d9f5b5ebebc
onelitefeather <schauderkristall@hotmail.de>
2026-08-10 20:57:16 +0200

    chore(config): remove publishing configuration from Gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,10 @@ plugins {
     alias(libs.plugins.shadow)
 }
 
+group = "net.onelitefeather"
+version = "1.2.10" // x-release-please-version
+description = "Labyrinth zone protection for OneLiteFeather - Prototype"
+
 dependencies {
     implementation(libs.cloudPaper)
     implementation(libs.cloudAnnotations)
@@ -49,55 +53,6 @@ tasks {
         jvmArgs("-Dlabyrinth.insideTest=true")
         testLogging {
             events("passed", "skipped", "failed")
-        }
-    }
-}
-
-publishing {
-    publications.create<MavenPublication>("maven") {
-        artifact(project.tasks.getByName("shadowJar"))
-        version = rootProject.version as String
-        artifactId = "labyrinth"
-        groupId = rootProject.group as String
-        pom {
-            name = "Labyrinth"
-            description = "Labyrinth zone protection for OneLiteFeather"
-            url = "https://github.com/OneLiteFeatherNET/labyrinth"
-            licenses {
-                license {
-                    name = "The Apache License, Version 2.0"
-                    url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-                }
-            }
-            developers {
-                developer {
-                    id = "themeinerlp"
-                    name = "Phillipp Glanz"
-                    email = "p.glanz@madfix.me"
-                }
-            }
-            scm {
-                connection = "scm:git:git://github.com:OneLiteFeatherNET/Labyrinth.git"
-                developerConnection = "scm:git:ssh://git@github.com:OneLiteFeatherNET/Labyrinth.git"
-                url = "https://github.com/OneLiteFeatherNET/labyrinth"
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            authentication {
-                credentials(PasswordCredentials::class) {
-                    // Those credentials need to be set under "Settings -> Secrets -> Actions" in your repository
-                    username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
-                    password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
-                }
-            }
-
-            name = "OneLiteFeatherRepository"
-            val releasesRepoUrl = uri("https://repo.onelitefeather.dev/onelitefeather-releases")
-            val snapshotsRepoUrl = uri("https://repo.onelitefeather.dev/onelitefeather-snapshots")
-            url = if (version.toString().contains("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
         }
     }
 }
